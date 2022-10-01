c6c0120c580c5e438fe2d7867d7fec617186c45d
Jaeuk Jeong <jaeuk@jeuke.com>
2022-10-01 17:12:14 +0900

    Proctect Resolver

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,42 +1,44 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import client from "../../client";
+import { protectResolver } from "../users.utils";
 
 export default {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, username, email, password: newPassword },
-      { loggedInUser }
-    ) => {
-      let hashedPassword = null;
-      if (newPassword) {
-        hashedPassword = await bcrypt.hash(newPassword, 10);
-      }
+    editProfile: protectResolver(
+      async (
+        _,
+        { firstName, lastName, username, email, password: newPassword },
+        { loggedInUser }
+      ) => {
+        let hashedPassword = null;
+        if (newPassword) {
+          hashedPassword = await bcrypt.hash(newPassword, 10);
+        }
 
-      const updatedUser = await client.user.update({
-        where: {
-          id: loggedInUser.id,
-        },
-        data: {
-          firstName,
-          lastName,
-          username,
-          email,
-          ...(hashedPassword && { password: hashedPassword }),
-        },
-      });
+        const updatedUser = await client.user.update({
+          where: {
+            id: loggedInUser.id,
+          },
+          data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            ...(hashedPassword && { password: hashedPassword }),
+          },
+        });
 
-      if (updatedUser.id) {
-        return {
-          ok: true,
-        };
-      } else {
-        return {
-          ok: false,
-          error: "Could not update profile.",
-        };
+        if (updatedUser.id) {
+          return {
+            ok: true,
+          };
+        } else {
+          return {
+            ok: false,
+            error: "Could not update profile.",
+          };
+        }
       }
-    },
+    ),
   },
 };
