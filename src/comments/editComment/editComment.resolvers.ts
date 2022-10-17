8f7ed36390ba1427dbb2ca27ce7a01502b2ec153
Jaeuk Jeong <jaeuk@jeuke.com>
2022-10-17 20:06:26 +0900

    Refactor MutationResult

---
+++ src/comments/editComment/editComment.resolvers.ts
@@ -0,0 +1,45 @@
+import e = require("express");
+import { Resolvers } from "../../types";
+import { protectedResolver } from "../../users/users.utils";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    editComment: protectedResolver(
+      async (_, { id, payload }, { loggedInUser, client }) => {
+        const comment = await client.comment.findUnique({
+          where: {
+            id,
+          },
+          select: {
+            userId: true,
+          },
+        });
+        if (!comment) {
+          return {
+            ok: false,
+            error: "Comment Not Found",
+          };
+        } else if (comment.userId !== loggedInUser.id) {
+          return {
+            ok: false,
+            error: "Not authorized.",
+          };
+        } else {
+          await client.comment.update({
+            where: {
+              id,
+            },
+            data: {
+              payload,
+            },
+          });
+          return {
+            ok: true,
+          };
+        }
+      }
+    ),
+  },
+};
+
+export default resolvers;
--- src/photos/uploadPhoto/uploadPhoto.typeDefs.ts
+++ src/photos/uploadPhoto/uploadPhoto.typeDefs.ts
@@ -1,12 +1,7 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
-  type UploadPhotoResult {
-    ok: Boolean!
-    error: String
-    result: Photo
-  }
   type Mutation {
-    uploadPhoto(file: String!, caption: String): UploadPhotoResult!
+    uploadPhoto(file: String!, caption: String): MutationResponse!
   }
 `;
